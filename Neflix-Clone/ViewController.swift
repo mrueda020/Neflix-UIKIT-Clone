0f4f66260ff5eeb56158235b8d8fd096dd2641fa
mrueda020 <mrueda020@hotmail.com>
2022-06-10 12:12:27 -0500

    Initial Commit

---
+++ Neflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neflix-Clone
+//
+//  Created by Miguel Rueda Carbajal on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Neflix-CloneUITests/Neflix_CloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Neflix_CloneUITestsLaunchTests.swift
+//  Neflix-CloneUITests
+//
+//  Created by Miguel Rueda Carbajal on 10/06/22.
+//
+
+import XCTest
+
+class Neflix_CloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
